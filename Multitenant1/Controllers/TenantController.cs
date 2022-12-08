7112874cd7c81c7400d1e367f354863d359a348e
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2022-12-08 09:44:29 +0300

    New Tenant

---
+++ Multitenant1/Controllers/TenantController.cs
@@ -0,0 +1,24 @@
+﻿using Core.Interfaces;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Multitenant.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TenantController : ControllerBase
+    {
+        private readonly ITenantService _service;
+
+        public TenantController(ITenantService service)
+        {
+            _service = service;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync()
+        {
+           return Ok();
+        }
+         
+    }
+}
